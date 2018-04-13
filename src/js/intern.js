5258c10715724cced8266677e39b97d3a8c66daf
Vincent Desdoigts <v.desdoigts@gmail.com>
2018-04-13 15:43:31 +0200

    TYPOGRAPHY - Update charte typos

---
+++ src/js/intern.js
@@ -0,0 +1,88 @@
+import 'bootstrap'
+import './utils/dragscroll.js'
+import './utils/stretchy.js'
+import * as tuichart from 'tui-chart'
+import {
+  LineChart,
+  RadialChart
+} from './components/charts'
+import Chips from './components/chips'
+import Control from './components/control'
+import Picker from './components/picker'
+import RangeSlider from './components/rangeSliders'
+import Searchbar from './components/searchbar'
+import SelectExclusive from './components/selectExclusive'
+import SelectMultiple from './components/selectMultiple'
+import SelectRadios from './components/selectRadios'
+
+const theme = {
+  series: {
+    colors: ['#0088ce']
+  }
+}
+tuichart.registerTheme('sncf', theme)
+
+document.addEventListener('DOMContentLoaded', () => {
+  /* eslint-disable no-console */
+  console.log('Hello from the Fab Design.')
+  /* eslint-enable no-console */
+
+  // data components
+  const dataComponent = '[data-component]'
+  const dataLineChart = 'line-chart'
+  const dataRadialChart = 'radial-chart'
+  const dataChips = 'chips'
+  const dataControl = 'control'
+  const dataPicker = 'picker'
+  const dataRangeSlider = 'range-slider'
+  const dataSearchbar = 'searchbar'
+  const dataSelectExclusive = 'select-exclusive'
+  const dataSelectMultiple = 'select-multiple'
+  const dataSelectRadios = 'select-radios'
+
+  const components = document.querySelectorAll(dataComponent)
+
+  components.forEach((component) => {
+    /* eslint-disable no-new */
+    if (component.dataset.component === dataLineChart) {
+      new LineChart(component)
+    }
+
+    if (component.dataset.component === dataRadialChart) {
+      new RadialChart(component)
+    }
+
+    if (component.dataset.component === dataChips) {
+      new Chips(component)
+    }
+
+    if (component.dataset.component === dataControl) {
+      new Control(component)
+    }
+
+    if (component.dataset.component === dataPicker) {
+      new Picker(component)
+    }
+
+    if (component.dataset.component === dataRangeSlider) {
+      new RangeSlider(component)
+    }
+
+    if (component.dataset.component === dataSearchbar) {
+      new Searchbar(component)
+    }
+
+    if (component.dataset.component === dataSelectExclusive) {
+      new SelectExclusive(component)
+    }
+
+    if (component.dataset.component === dataSelectMultiple) {
+      new SelectMultiple(component)
+    }
+
+    if (component.dataset.component === dataSelectRadios) {
+      new SelectRadios(component)
+    }
+    /* eslint-enable no-new */
+  })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
-const theme = process.env.theme || 'extern';
+const theme = process.env.theme || 'intern';
 
 module.exports = {
   entry: [
