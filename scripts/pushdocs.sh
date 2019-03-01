0e17557554aadbd6c3153fe454f98905ffb28b5f
Nicolas Wurtz <nicolas.wurtz@sncf.fr>
2019-03-01 11:18:46 +0100

    [TRAVIS] Tests - maj script deploy doc token

--- scripts/pushdocs.sh
+++ scripts/pushdocs.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-# Adjusts git deploy repo, or gets it if unavailable
+# Adjusts git deploy repo
+GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO}
+
 cd documentation && \
 git init && \
 git config user.name "Travis CI" && \
 git config user.email "github@travis-ci.org" && \
 git add . && \
 git commit -m "Deploy to GitHub Pages" && \
-git push --force "https://github.com/SNCFdevelopers/bootstrap-sncf" master:gh-pages
+git push --force "${GIT_DEPLOY_REPO}" master:gh-pages
