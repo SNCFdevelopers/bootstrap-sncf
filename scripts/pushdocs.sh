ce06ee300b912decce3e8e334e06fd7601a0b260
Nicolas Wurtz <nicolas.wurtz@sncf.fr>
2019-03-01 15:18:35 +0100

    [TRAVIS][DOC] Go to bootstrap-sncf-doc-(int|-ext)

--- scripts/pushdocs.sh
+++ scripts/pushdocs.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
 # Adjusts git deploy repo
-GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO}
+GIT_DEPLOY_DOC_INT=${GIT_DEPLOY_DOC_INT}
+GIT_DEPLOY_DOC_EXT=${GIT_DEPLOY_DOC_EXT}
 
-cd documentation && \
+cd documentation/intern && \
 git init && \
 git config user.name "Travis CI" && \
 git config user.email "github@travis-ci.org" && \
 git add . && \
-git commit -m "Deploy to GitHub Pages" && \
-git push --force "${GIT_DEPLOY_REPO}" master:gh-pages
+git commit -m "Deploy to GitHub Pages - Metier" && \
+git push --force "${GIT_DEPLOY_DOC_INT}" master:gh-pages
+
+cd documentation/extern && \
+git init && \
+git config user.name "Travis CI" && \
+git config user.email "github@travis-ci.org" && \
+git add . && \
+git commit -m "Deploy to GitHub Pages - Communication" && \
+git push --force "${GIT_DEPLOY_DOC_EXT}" master:gh-pages
