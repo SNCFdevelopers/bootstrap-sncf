b0f22793602e7ac8ef778e4ea92dbe788da46f8d
Nicolas Wurtz <nicolas.wurtz@sncf.fr>
2019-03-01 09:48:20 +0100

    [TRAVIS] Tests - mise à jour script deploiement doc

--- scripts/pushdocs.sh
+++ scripts/pushdocs.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 # Adjusts git deploy repo, or gets it if unavailable
-GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(node -e 'process.stdout.write(require("./package.json").repository)')}
-
 cd documentation && \
 git init && \
 git config user.name "Travis CI" && \
 git config user.email "github@travis-ci.org" && \
 git add . && \
 git commit -m "Deploy to GitHub Pages" && \
-git push --force "${GIT_DEPLOY_REPO}" master:gh-pages
+git push --force "https://github.com/SNCFdevelopers/bootstrap-sncf" master:gh-pages
