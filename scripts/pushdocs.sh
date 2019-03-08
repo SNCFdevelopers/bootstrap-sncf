123954ae3c30ab63fd9d1a7da05f6dd412ca27ef
Nicolas Wurtz <nicolas.wurtz@sncf.fr>
2019-03-08 08:28:51 +0100

    [TRAVIS] Test - pushdoc problems

--- scripts/pushdocs.sh
+++ scripts/pushdocs.sh
@@ -5,7 +5,6 @@ GIT_DEPLOY_DOC_INT=${GIT_DEPLOY_DOC_INT}
 GIT_DEPLOY_DOC_EXT=${GIT_DEPLOY_DOC_EXT}
 
 cd documentation/intern && \
-echo "bootstrap-int.fd.sncf.fr" > CNAME \
 git init && \
 git config user.name "Travis CI" && \
 git config user.email "github@travis-ci.org" && \
@@ -14,7 +13,6 @@ git commit -m "Deploy to GitHub Pages - Metier" && \
 git push --force "${GIT_DEPLOY_DOC_INT}" master:gh-pages
 
 cd ../extern && \
-echo "bootstrap-ext.fd.sncf.fr" > CNAME \
 git init && \
 git config user.name "Travis CI" && \
 git config user.email "github@travis-ci.org" && \
