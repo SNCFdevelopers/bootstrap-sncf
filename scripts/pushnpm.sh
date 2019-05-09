fd4f8dae0d3b6fa9a07eae664d5a9ebe91903d40
Nicolas Wurtz <nicolas.wurtz@sncf.fr>
2019-05-09 17:32:19 +0200

    [TRAVIS] Issues with npm

--- scripts/pushnpm.sh
+++ scripts/pushnpm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-NPM_TOKEN=${NPM_AUTH_TOKEN}
+export NPM_TOKEN=${NPM_AUTH_TOKEN}
 if [ "$TRAVIS_BRANCH" = "master" ]; then 
-  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
+  echo "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" > .npmrc
   sed s#"@sncf/bootstrap-sncf"#"@sncf/bootstrap-sncf.$1"# -i package.json
   npm publish --access public
   sed s#"@sncf/bootstrap-sncf.$1"#"@sncf/bootstrap-sncf"# -i package.json
