50ceb2a40be16c055e6f62f10cc0976402ec56f6
Nicolas Wurtz <nicolas.wurtz@sncf.fr>
2019-05-09 17:43:07 +0200

    [TRAVIS] Issues with npm 2

--- scripts/pushnpm.sh
+++ scripts/pushnpm.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 export NPM_TOKEN=${NPM_AUTH_TOKEN}
 if [ "$TRAVIS_BRANCH" = "master" ]; then 
-  echo "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" > .npmrc
+  echo "name=@sncf/bootstrap-sncf.$1" > .npmrc
+  echo "auth=${NPM_AUTH_TOKEN}" >> .npmrc
+  echo "email=nicolas.wurtz@sncf.fr" >> .npmrc
   sed s#"@sncf/bootstrap-sncf"#"@sncf/bootstrap-sncf.$1"# -i package.json
   npm publish --access public
   sed s#"@sncf/bootstrap-sncf.$1"#"@sncf/bootstrap-sncf"# -i package.json
