9a32975c6bb172afb806b1e2a075ac2ffce96602
Nicolas Wurtz <nicolas.wurtz@sncf.fr>
2019-05-09 17:03:58 +0200

    [TRAVIS][NPM] Script corrected

--- scripts/pushnpm.sh
+++ scripts/pushnpm.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
-
 NPM_TOKEN=${NPM_AUTH_TOKEN}
-
-sed s#"@sncf/bootstrap-sncf"#"@sncf/bootstrap-sncf.$1"# -i package.json
-npm publish --access public
-sed s#"@sncf/bootstrap-sncf.$1"#"@sncf/bootstrap-sncf"# -i package.json
+if [ "$TRAVIS_BRANCH" = "master" ]; then 
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
+  sed s#"@sncf/bootstrap-sncf"#"@sncf/bootstrap-sncf.$1"# -i package.json
+  npm publish --access public
+  sed s#"@sncf/bootstrap-sncf.$1"#"@sncf/bootstrap-sncf"# -i package.json
+fi
